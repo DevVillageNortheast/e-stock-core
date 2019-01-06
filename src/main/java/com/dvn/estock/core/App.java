f5a8435707f0bb14e411d113f672cffac6e988e9
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2019-01-06 16:26:17 -0300

    Maven project init

---
+++ src/main/java/com/dvn/estock/core/App.java
@@ -0,0 +1,13 @@
+package com.dvn.estock.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
