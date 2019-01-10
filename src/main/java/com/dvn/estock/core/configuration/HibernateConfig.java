4fcb783ede17d0ade9014c8098a72a358377aba7
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2019-01-10 10:30:34 -0300

    Algumas alterações na estrutura do projeto e wrapper do maven esta sendo utilizado

--- src/main/java/com/dvn/estock/core/configuration/HibernateConfig.java
+++ src/main/java/com/dvn/estock/core/configuration/HibernateConfig.java
@@ -13,48 +13,46 @@ import org.hibernate.cfg.Environment;
 public class HibernateConfig{
 
     private static StandardServiceRegistry registry;
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+        try{
+            // Create registry builder
+            StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+            // Hibernate settings equivalent to hibernate.cfg.xml's properties
+            Map<String, String> settings = new HashMap<String,String>();
+            
+            settings.put(Environment.DRIVER, DatabaseConfig.getDrive());
+            settings.put(Environment.URL, DatabaseConfig.getUrl());
+            settings.put(Environment.USER, DatabaseConfig.getUser());
+            settings.put(Environment.PASS, DatabaseConfig.getPassword());
+            settings.put(Environment.DIALECT, DatabaseConfig.getDialect());
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+            // Apply settings
+            registryBuilder.applySettings(settings);
+
+            // Create registry
+            registry = registryBuilder.build();
+
+            // Create MetadataSources
+            MetadataSources sources = new MetadataSources(registry);
+
+            // Create Metadata
+            Metadata metadata = sources.getMetadataBuilder().build();
+
+            // Create SessionFactory
+            return metadata.getSessionFactoryBuilder().build();
+
+        }catch(Throwable e){
+            System.err.println("Falha ao inicial a SessionFactory." + e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
     public static SessionFactory getSessionFactory(){
-
-        if(sessionFactory == null){
-            try{
-                // Create registry builder
-                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
-
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
-                Map<String, String> settings = new HashMap<String,String>();
-                
-                settings.put(Environment.DRIVER, DatabaseConfig.getDrive());
-                settings.put(Environment.URL, DatabaseConfig.getUrl());
-                settings.put(Environment.USER, DatabaseConfig.getUser());
-                settings.put(Environment.PASS, DatabaseConfig.getPassword());
-                settings.put(Environment.DIALECT, DatabaseConfig.getDialect());
-                settings.put(Environment.SHOW_SQL, "true");
-                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
-
-                // Apply settings
-                registryBuilder.applySettings(settings);
-
-                // Create registry
-                registry = registryBuilder.build();
-
-                // Create MetadataSources
-                MetadataSources sources = new MetadataSources(registry);
-
-                // Create Metadata
-                Metadata metadata = sources.getMetadataBuilder().build();
-
-                // Create SessionFactory
-                sessionFactory = metadata.getSessionFactoryBuilder().build();
-
-            }catch(Exception e){
-                e.printStackTrace();
-                if (registry != null) {
-                   StandardServiceRegistryBuilder.destroy(registry);
-                }
-            }
-        }
         return sessionFactory;
     }
 
--- src/test/java/com/dvn/estock/core/AppTest.java
+++ src/test/java/com/dvn/estock/core/AppTest.java
@@ -1,30 +1,8 @@
 package com.dvn.estock.core;
 
-import com.dvn.estock.core.configuration.DatabaseConfig;
-import com.dvn.estock.core.configuration.HibernateConfig;
-import com.dvn.estock.core.util.ConstantEnv;
-
-import org.hibernate.Session;
-
 public class AppTest {
 
     public static void main(String[] args) {
-
-        Session session = HibernateConfig.getSessionFactory().openSession();
-        session.beginTransaction();
-
-        String sql = "select version()";
-
-        String result = (String) session.createNativeQuery(sql).getSingleResult();
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        System.out.println(result);
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-
-        session.getTransaction().commit();
-
-        HibernateConfig.shutdown();
-
-        System.out.println(DatabaseConfig.getDialect());
-        System.out.println(ConstantEnv.getDatabaseConfEnv());
+        System.out.println("Olá mundo");
     }
 }
